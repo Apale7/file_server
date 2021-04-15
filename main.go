04c61131bd2def301fde86030e76ec4e0a6c7ebb
Apale7 <1092377056@qq.com>
2021-04-15 21:46:54 +0800

    config port by env

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	addr string
+)
+
+func init() {
+	addr = os.Getenv("file_server_addr")
+	if addr == "" {
+		addr = ":7777"
+	}
+}
+
 func main() {
 	router := gin.Default()
-	defer router.Run(":7777")
+	defer router.Run(addr)
 	register(router)
 }
